91474727364bcf99508a26dc5d2be5239a354c6e
john-rocky <john-rocky@hubot.com>
2021-10-20 03:32:13 +0900

    Initial Commit

---
+++ VisionSampler/VisionSamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionSamplerApp.swift
+//  VisionSampler
+//
+//  Created by 間嶋大輔 on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct VisionSamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
